a6110f341e84b752a6ae21dcce3df65f5e4c60e8
Fabio Hisamoto <fabio@99taxis.com>
2016-04-01 12:55:45 -0300

    Renders demo page.

--- web.rb
+++ web.rb
@@ -3,7 +3,5 @@ require 'sinatra'
 set :port, ENV["PORT"] || 5000
 
 get '/' do
-  whom = ENV["POWERED_BY"] || "Deis"
-  container = `hostname`.strip || "unknown"
-  "Powered by " + whom + "\nRunning on container ID " + container + "\n"
+  erb :index
 end
